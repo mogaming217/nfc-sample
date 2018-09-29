14b899b472ec092fa7797204c227e4a71d5318b4
seiya-mogami <seiya.mogami@dena.com>
2018-09-29 22:20:06 +0900

    Initial Commit

---
+++ nfc-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nfc-sample
+//
+//  Created by Seiya Mogami on 2018/09/29.
+//  Copyright © 2018年 Seiya Mogami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
